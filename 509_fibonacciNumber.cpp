4cb9f8947b76f7b1faf745344e71b3e8944f7411
itxsoumya <code.mrsp@gmail.com>
2022-09-02 15:20:04 +0530

    509fibo

---
+++ 509_fibonacciNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+
+int fibo(int n){
+    if (n==0 || n==1)
+    {
+        return n;
+    }
+    return fibo(n-1)+fibo(n-2);
+    
+}
+int main(){
+    std::cout<<fibo(4)<<std::endl;
+    return 0;
+}
